f08673853e2c66c338934913ce0b353afee17d3f
wangmeng <wangmeng@icarbonx.com>
2025-10-13 16:45:35 +0800

    deploy on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap"
 import icon from "astro-icon";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://neonmint.efeele.dev",
+
   integrations: [preact(), icon(), sitemap({
     filter: (page) =>
       !page.includes("/blog/tags") &&
@@ -17,9 +20,12 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark'
     },
   },
+
+  adapter: vercel(),
 });
